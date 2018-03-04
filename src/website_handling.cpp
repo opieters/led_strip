0c487a1271da5b92f8cab9fe21b684eb9984809c
Olivier Pieters <me@olivierpieters.be>
2018-03-04 21:43:45 +0100

    Improved code.

--- src/website_handling.cpp
+++ src/website_handling.cpp
@@ -1,12 +1,14 @@
 #include "website_handling.h"
+#include <NeoPixelBrightnessBus.h> 
 
 extern ESP8266WebServer server;
 
+void set_leds(RgbwColor color);
+
 void configure_website_pages(){
     server.serveStatic("/", SPIFFS, "/index.html");
-    server.serveStatic("/", SPIFFS, "/index.html");
 
-    server.on("/leds", HTTP_POST, handleLEDs);
+    server.on("/", HTTP_POST, handleLEDs);
 }
 
 
@@ -15,5 +17,15 @@ void handleLEDs(){
         server.send(400, "text/plain", "400: Invalid Request");
         return;
     }
+
+    // parse LED arguments
+    String red_s = server.arg("red"), green_s = server.arg("green"), blue_s = server.arg("blue"), white_s = server.arg("white");
+
+    uint8_t red = red_s.toInt(), green = green_s.toInt(), blue = blue_s.toInt(), white = white_s.toInt();
+
+    set_leds(RgbwColor(red, green, blue, white)); 
     
+    // redirect to main page
+    server.sendHeader("Location", String("/"), true);
+    server.send( 302, "text/plain", "");
 }
